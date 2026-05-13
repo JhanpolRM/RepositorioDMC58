f889b8e474f4616ffd4f4c99415a2b5d7ebf63c0
JhanpolRM <jhanpolrm@gmail.com>
2026-05-12 19:59:14 -0500

    Update app.py

--- app.py
+++ app.py
@@ -7,3 +7,16 @@ st.sidebar.title("Parámetros")
 st.write("Elaborado por: Jhanpol Rosales M.")
 
 sesion = st.sidebar.selectbox("Seleccione una Sesión", ["Sesión 01", "Sesión 02", "Sesión 03", "Sesión 04"])
+
+if sesion == "Sesión 01":
+  st.write("Bienvenido a la sesión 01")
+  st.image("Image20260512194055")
+  
+
+elif sesion=="Sesión 02":
+  st.write("Bienvenido a la sesión 02")
+elif sesion=="Sesión 03":
+  st.write("Bienvenido a la sesión 03")
+else
+  st.write("Bienvenido a la sesión 04")
+
