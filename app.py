f5a71943fe84f81d574cfc3d4e432d3fb89f961c
JhanpolRM <jhanpolrm@gmail.com>
2026-05-12 19:47:56 -0500

    Update app.py

--- app.py
+++ app.py
@@ -5,3 +5,5 @@ st.title("Mi primera aplicación en Python")
 st.sidebar.title("Parámetros")
 
 st.write("Elaborado por: Jhanpol Rosales M.")
+
+sesion = st.selectbox("Seleccione una Sesión", ["Sesión 01", "Sesión 02", "Sesión 03", "Sesión 04"])
